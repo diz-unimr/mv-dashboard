ac5145a347d524d435309f17b0221a90be939e70
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-04-24 14:31:16 +0200

    refactor: use clap v4 style attributes

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
 
 #[derive(Parser, Default)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 pub struct Config {
-    #[clap(
+    #[arg(
         long,
         env = "LISTEN",
         default_value = "0.0.0.0:3000",
@@ -11,7 +11,7 @@ pub struct Config {
     )]
     pub listen: String,
 
-    #[clap(
+    #[arg(
         long,
         env = "ONKOSTAR_URL",
         default_value = "http://localhost:8080/onkostar",
@@ -19,14 +19,14 @@ pub struct Config {
     )]
     pub onkostar_url: String,
 
-    #[clap(
+    #[arg(
         long,
         env = "COOKIE_DOMAIN",
         help = "The cookie domain to be used (optional)"
     )]
     pub cookie_domain: Option<String>,
 
-    #[clap(
+    #[arg(
         long,
         env = "CACHE_DURATION",
         help = "Enable response caching with the given duration (optional)"
